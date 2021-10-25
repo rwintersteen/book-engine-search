5b6c505c2db981d25403b53d32845a2345ab81a0
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-24 22:54:44 -0700

    Added login, save book, and remove book mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,7 +20,45 @@ const resolvers = {
             const token = signToken(user);
 
             return { token, user };
-        }
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                throw new AuthenticationError('No user exists with this email found!');
+            }
+
+            const correctPw = await profile.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Sorry, incorrect password! Try Again!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { userId, book }) => {
+            return User.findOneAndUpdate(
+                { _id: userId },
+                {
+                  $addToSet: { books: book }
+                },
+                {
+                  new: true,
+                  runValidators: true,
+                }
+            );
+        },
+       
+        removeBook: async (parent, { userId, book }) => {
+            return User.findOneAndUpdate(
+                { _id: userId },
+                { $pull: { books: book } },
+                { new: true }
+            );
+        },
+    },
     },
 };
 
