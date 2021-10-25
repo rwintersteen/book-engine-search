1b790960a3446c754b141d320401edfb067e9755
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-24 22:43:42 -0700

    Inputed typeDefs from book and user js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User  
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: String!, bookAuthor: String!): User
+    removeBook(BookIdId: ID!): User
+  }
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: String!
+    savedBooks: [Book] 
+  }
+  type Book {
+      bookId: ID
+      authors: [String]
+      description: String!
+      title: String!
+      image: String!
+      link: String!
+  }
+  input BookInput {
+      authors: [String]
+      description: String!
+      image: String!
+      title: String!
+      link: String!
+      bookId: String!
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -26,5 +26,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
