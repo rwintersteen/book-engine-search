caff0cda1c56f36cd46d03ce109cd505eeb0218a
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-24 22:51:48 -0700

    addUser mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,15 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {},
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+            const user = await User.create({ name, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        }
+    },
 };
 
 module.exports = resolvers;
