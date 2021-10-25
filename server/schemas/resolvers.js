04563218018f17800b019f6ebdddf1950eef7718
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-24 22:53:40 -0700

    Added query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,7 +4,15 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {},
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userData
+            }
+        }
+    },
 
     Mutation: {
         addUser: async (parent, { name, email, password }) => {
