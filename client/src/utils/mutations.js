397bd07f2c9b8f895954ad6a55bf1e28627a558e
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-24 23:18:42 -0700

    Completed mutations for front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,51 @@ export const LOGIN_USER = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: String!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks{
+          bookId
+          authors
+          title
+          description
+          link
+          image
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        savedBooks{
+            bookId
+            authors
+            title
+            description
+            link
+            image
+        }
+    }
+  }
+`;
