4dd7589ac90ffbbad9a638b8951cc9ffb303925a
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-24 22:46:42 -0700

    Started resovlers, will need to work on query and mutations

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,10 @@
+
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {},
+};
+
+module.exports = resolvers;
