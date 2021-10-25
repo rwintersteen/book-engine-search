b82f2eb60b9f72a918bd642948747304612ec4aa
Rachael Wintersteen <rachaelwintersteen@gmail.com>
2021-10-24 22:09:50 -0700

    Re-intialized code after experiencing too many errors

--- server/server.js
+++ server/server.js
@@ -1,25 +1,10 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-// importing typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// creating new Apollo-Server, passes through schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-})
-
-server.appplyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -29,17 +14,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-});
-
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
 });
